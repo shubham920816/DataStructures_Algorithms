971bbe231a61b109aab4ec3dc2598461b40d0b65
shubham920816 <shubhaM1674@>
2020-09-20 01:00:32 +0530

    Commit

--- MIT6006/peakfinder1d.py
+++ MIT6006/peakfinder1d.py
@@ -9,28 +9,36 @@ class PeakFinder(object):
         self.input = input_val
 
     def _logic(self, array=[]):
+        """
+
+        Args:
+            array:
+
+        Returns:
+
+        """
         n = len(array)
         mid = int(n / 2)
-        for i in range(0, n):
-            if n > 2:
-                if (array[mid] >= array[mid + 1]) and (array[mid] >= array[(mid) - 1]):
-                    value = array[mid]
-                    return value
 
-                elif array[mid - 1] >= array[mid]:
-                    temp_array = array[:mid]
-                    return self._logic(temp_array)
-                elif array[mid + 1] >= array[mid]:
-                    temp_array = array[mid:]
-                    return self._logic(temp_array)
-            elif n == 1:
+        if n > 2:
+            if (array[mid] >= array[mid + 1]) and (array[mid] >= array[(mid) - 1]):
                 value = array[mid]
                 return value
+
+            elif array[mid - 1] >= array[mid]:
+                temp_array = array[:mid]
+                return self._logic(temp_array)
+            elif array[mid + 1] >= array[mid]:
+                temp_array = array[mid:]
+                return self._logic(temp_array)
+        elif n == 1:
+            value = array[mid]
+            return value
+        else:
+            if array[mid] > array[mid - 1]:
+                return array[mid]
             else:
-                if array[mid] > array[mid - 1]:
-                    return array[mid]
-                else:
-                    return array[mid - 1]
+                return array[mid - 1]
 
     def run_peak_finder(self):
         """
@@ -45,5 +53,5 @@ class PeakFinder(object):
 if __name__ == "__main__":
 
     val = [1, 5, 35, 7, 8, 15, 20, 25, 30]
-    t1 = PeakFinder(input=val)
+    t1 = PeakFinder(input_val=val)
     print(t1.run_peak_finder())
